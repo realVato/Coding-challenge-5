1c3ebbd48b981835cd4373d1b78cc6c884a23e88
realVato <44781061+realVato@users.noreply.github.com>
2019-06-15 19:48:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,117 +1,79 @@
-// Calculating John's family expenses:
-
-var JOHN_FAMILY = {
-
-    tips: [],
-    finalBill: [],
-    bills: [124, 48, 268, 180, 42],
-    calculateTip: function() {
-
-        for (var i = 0; i < this.bills.length; i++) {
-            
-            if (this.bills[i] < 50) {
-
-                this.tips[i] = this.bills[i] * .2;
-                
-            } else if (this.bills[i] > 50 && this.bills[i] < 200) {
-
-                this.tips[i] = this.bills[i] * .15;
-               
-            } else {
-
-                this.tips[i] = this.bills[i] * .1;
-                
-            }
-
-            this.finalBill[i] = this.bills[i] + this.tips[i];
-        }
-    },
-
-    tipsSum: function() {
-
-        tipSum = 0;
-
-        for (var i = 0; i < this.bills.length; i++) {
-
-            tipSum += this.tips[i];
-        }
-        return tipSum;
-    }
-}
-
-
-JOHN_FAMILY.calculateTip();
-
-
-// Calculating Mark's family expenses:
-
-var MARK_FAMILY = {
-
-    tips: [],
-    finalBill: [],
-    bills: [77, 375, 110, 45],
-    calculateTip: function() {
-
-        for (var i = 0; i < this.bills.length; i++) {
-            
-            if (this.bills[i] < 100) {
-
-                this.tips[i] = this.bills[i] * .2;
-                
-            } else if (this.bills[i] > 100 && this.bills[i] < 300) {
-
-                this.tips[i] = this.bills[i] * .1;
-               
-            } else {
-
-                this.tips[i] = this.bills[i] * .25;
-                
-            }
-
-            this.finalBill[i] = this.bills[i] + this.tips[i];
-        }
-    },
-
-    tipsSum: function() {
-
-        tipSum = 0;
-
-        for (var i = 0; i < this.bills.length; i++) {
-
-            tipSum += this.tips[i];
-        }
-        return tipSum;
-    }
-}
-
-MARK_FAMILY.calculateTip();
-
-
-//Calculating the average tips of each family:
-
-JOHN_FAMILY_AVG = 0;
-MARK_FAMILY_AVG = 0;
-
-function avgFamilyTip () {
-
-    JOHN_FAMILY_AVG = JOHN_FAMILY.tipsSum() / JOHN_FAMILY.tips.length;
-    MARK_FAMILY_AVG = MARK_FAMILY.tipsSum() / MARK_FAMILY.tips.length;
-
-    if (JOHN_FAMILY_AVG > MARK_FAMILY_AVG) {
-
-        console.log('John\'s family have tipped more!');
-
-    } else if (JOHN_FAMILY_AVG < MARK_FAMILY_AVG) {
-
-        console.log('Mark\'s family have tipped more!');
-
+function calculateTipJohn(bill) {
+    if (bill < 50) {
+        return bill * .2;
+    } else if (bill >= 50 && bill <= 200) {
+        return bill * .15;
     } else {
-
-        console.log('Both families have tipped the same amount!');
-
+        return bill * .1;
     }
 }
 
-avgFamilyTip();
+var john = {
+    bills: [124, 48, 268, 188, 42],
+    tips: [],
+    finalBills: [],
+    calcBill: function() {
+        for (var i = 0; i < this.bills.length; i++) {
+            this.tips[i] = calculateTipJohn(this.bills[i]);
+            this.finalBills[i] = this.bills[i] + this.tips[i];
+        }
+    }
+}
 
+john.calcBill();
+// console.log(john.tips);
+// console.log(john.finalBills);
 
+function calculateTipMark(bill) {
+    if (bill < 100) {
+        return bill * .2;
+    } else if (bill >= 100 && bill <= 300) {
+        return bill * .1;
+    } else {
+        return bill * .25;
+    }
+}
+
+var mark = {
+    bills: [77, 375, 110, 45],
+    tips: [],
+    finalBills: [],
+    calcBill: function() {
+        for (var i = 0; i < this.bills.length; i++) {
+            this.tips[i] = calculateTipMark(this.bills[i]);
+            this.finalBills[i] = this.bills[i] + this.tips[i];
+        }
+    }
+}
+
+mark.calcBill();
+// Extra
+console.log(mark.tips);
+
+function markTips() {
+    var markTipSum = 0;
+    for (var i = 0; i < mark.tips.length; i++) {
+        markTipSum = markTipSum + mark.tips[i];
+    }
+    return markTipSum / mark.tips.length;
+}
+
+function johnTips() {
+    var johnTipSum = 0;
+    for (var i = 0; i < john.tips.length; i++) {
+        johnTipSum = johnTipSum + john.tips[i];
+    }
+    return johnTipSum / john.tips.length;
+}
+
+function highestAvg() {
+    if (markTips() > johnTips()) {
+        console.log('Mark has better avg');
+    }  else {
+        console.log('john has better avg');
+    }
+}
+
+console.log(markTips());
+console.log(johnTips());
+highestAvg();
